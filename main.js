7573f52c5288ba58f2718e840576dbe6e3907b73
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-09-14 15:44:17 -0300

    Finalização do arquivos

---
+++ main.js
@@ -0,0 +1,32 @@
+async function buscaEndereco(cep) {
+	try {
+    var mensagemDeErro = document.querySelector('.erro');
+    mensagemDeErro.innerHTML = '';
+    console.log(mensagemDeErro);
+	  var consultaCep = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+	  var consultaCepParaJson = await consultaCep.json();
+
+    var rua = document.querySelector('#rua');
+    var cidade = document.querySelector('#cidade');
+  
+    rua.value = consultaCepParaJson.logradouro;
+    bairro.value = consultaCepParaJson.bairro;
+    cidade.value = consultaCepParaJson.localidade;
+    estado.value = consultaCepParaJson.uf;
+
+    return consultaCepParaJson
+
+	} catch(erro) {
+    mensagemDeErro.innerHTML = `<span>Cep inválido! Tente novamente.</span>`;
+    mensagemDeErro.classList.add('erro__texto');
+	  
+	}
+
+}
+
+
+
+var campoCep = document.querySelector('#cep');
+campoCep.addEventListener('focusout', () => buscaEndereco(campoCep.value));
+
+
